fcec6bc332f1da43cdc9bced8efbe12b1f4e267c
Marcel Bautista-Kuljevan <mrebg@hotmail.fr>
2026-09-17 17:31:29 +0200

    style: load word search styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./validation.css";
+import "./search.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
